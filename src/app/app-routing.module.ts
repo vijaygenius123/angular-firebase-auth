45fb20819588f7d453fbf76bce476d965c3cb95f
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-10-27 19:04:27 +0100

    Add AuthGaurd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./components/login/login.component";
+import {HomeComponent} from "./components/home/home.component";
+import {AuthGuard} from "./services/auth.guard";
 
 
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', component: HomeComponent, canActivate: [AuthGuard], pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,24 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
-import {RouterModule, Routes} from "@angular/router";
 import {AngularFireModule} from "@angular/fire/compat";
 import {AngularFireAuthModule} from "@angular/fire/compat/auth";
 import {environment} from "../environments/environment";
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = []
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes),
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule,
   ],
